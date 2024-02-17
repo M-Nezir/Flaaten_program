dcd8d9ac6cccc724a2c714b52d1482735d1830a1
M-Nezir <146748647+M-Nezir@users.noreply.github.com>
2024-02-17 16:49:31 +0300

    Update oneproject.py

--- oneproject.py
+++ oneproject.py
@@ -1,6 +1,5 @@
 """"
-Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi)
-oluşabileceği gibi, non-scalar verilerden de oluşabilir. Örnek olarak:
+İç içe geçmiş veri yapılarını düzleştiren Flatten Programı
 
 input: [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 
